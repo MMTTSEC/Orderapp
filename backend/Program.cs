d88a2bb7f76e96555a5038c779969e62bd56ab2e
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-10-22 10:02:44 +0200

    Implement authentication endpoints for user registration and login

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,28 +1,22 @@
 using OrchardCore.Logging;
+using backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseNLogHost();
 
-builder.Services
-    .AddOrchardCms()
-    // // Orchard Specific Pipeline
-    // .ConfigureServices( services => {
-    // })
-    // .Configure( (app, routes, services) => {
-    // })
-;
+builder.Services.AddOrchardCms();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.MapAuthEndpoints();
+
 app.UseStaticFiles();
 
 app.UseOrchardCore();
