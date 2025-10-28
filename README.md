b205fed83b74241a0550f072ab252a0d796c76b0
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-10-28 10:48:39 +0100

    Remove project

--- README.md
+++
@@ -1,2 +0,0 @@
-# My web app
-The start of my web app. Here I will document my database, the REST-api, my components.
--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-export default function App() {
-  return <>
-    <h1>Hello World!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo sequi nulla maxime possimus praesentium repellat? Aliquid labore pariatur ullam, magnam eligendi expedita fugit, suscipit ipsam repellendus numquam possimus.</p>
-  </>;
-}
