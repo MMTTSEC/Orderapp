be2acd3fdb4694606e693c3ca6596df84e9fcacc
Pontus Flodström <pontaflod95@hotmail.com>
2025-11-03 12:28:44 +0100

    updated Order Index with styled buttons.

--- src/pages/order-pages/OrderIndex.tsx
+++ src/pages/order-pages/OrderIndex.tsx
@@ -5,7 +5,9 @@ OrderIndex.route = {
 export default function OrderIndex() {
 
   return <>
-    <h1>Order Index</h1>
-    <p>Let's start building the app.</p>
+    <div className="main-container order-index-page">
+      <button className="btn-left"><i className="bi bi-fork-knife"></i>PÅ PLATS</button>
+      <button className="btn-right"><i className="bi bi-bag-fill"></i>TA MED</button>
+    </div>
   </>
 }
