adbaa9a807d7e278cacacbd846a14b5bb457e7f1
Pontus Flodström <pontaflod95@hotmail.com>
2025-11-03 12:36:46 +0100

    added useNavigate from react-router-dom to navigate the user to /order-selection when clicking on any of the two buttons.

--- src/pages/order-pages/OrderIndex.tsx
+++ src/pages/order-pages/OrderIndex.tsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 OrderIndex.route = {
   path: '/'
 };
 
 export default function OrderIndex() {
+  const navigate = useNavigate();
+  const goToOrderSelection = () => {
+    navigate("/order-selection");
+  };
 
   return <>
     <div className="main-container order-index-page">
-      <button className="btn-left"><i className="bi bi-fork-knife"></i>PÅ PLATS</button>
-      <button className="btn-right"><i className="bi bi-bag-fill"></i>TA MED</button>
+      <button className="btn-left" onClick={goToOrderSelection}><i className="bi bi-fork-knife"></i>PÅ PLATS</button>
+      <button className="btn-right" onClick={goToOrderSelection}><i className="bi bi-bag-fill"></i>TA MED</button>
     </div>
   </>
 }
